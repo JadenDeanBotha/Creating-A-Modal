324bd30376bddf33c68ba0c6732e1c08ea009026
JadenDeanBotha <138444240+JadenDeanBotha@users.noreply.github.com>
2023-10-16 18:48:44 +0200

    uploaded app

---
+++ Day4-CreateAmodal/script.js
@@ -0,0 +1,26 @@
+//Variables
+const modal = document.querySelector(".modal"),
+  btn = document.querySelector(".btn"),
+  close = document.querySelector(".close"),
+  modalContent = document.querySelector(".modal-content")
+
+
+btn.addEventListener("click", openModal);
+close.addEventListener("click", closeModal);
+modal.addEventListener("click", closeModal);
+
+// Open Modal
+function openModal(e) {
+    e.preventDefault();
+    modal.style.display = "block";
+}
+
+// Close Modal
+function closeModal() {
+    modalContent.classList.add("slide-up");
+
+    setTimeout(() => {
+        modal.style.display = "none";
+        modalContent.classList.remove("slide-up");
+    }, 500)
+}
